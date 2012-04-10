02a29b3e1735bb7766152b9c06fac93318eb7b4b
Torsten Stremlau <TerrySoba@users.noreply.github.com>
2012-04-10 02:38:07 +0200

    - added polygon collisions

--- src/MathUtils.h
+++ src/MathUtils.h
@@ -18,4 +18,29 @@ inline T abs(T value) {
 	return (value >= 0)?value:-value;
 }
 
+/*! \brief calculates positive modulus
+ *
+ *  This function computes the positive modulo value.
+ *  Usually a modulus calculation in C++ preserves the sign of the value.
+ *  e.g.  -1 % 3 == -1
+ *
+ *  This function always returns a positive value. In the example given
+ *  above this function yields:
+ *        positiveModulo(-1, 3) == 2
+ *
+ *  \param value the numerator
+ *  \param modulo the denominator
+ *
+ *  \return the rest of the integer division
+ */
+int32_t positiveModulo(int32_t value, int32_t modulo) {
+	int32_t tmp = value % modulo;
+	if (tmp >= 0) {
+		return tmp;
+	} else {
+		return tmp + modulo;
+	}
+}
+
+
 #endif /* MATHUTILS_H_ */
