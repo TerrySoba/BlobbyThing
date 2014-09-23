21fbbfb9291e41061c8ad8702adbb798a3fd578b
Torsten Stremlau <TerrySoba@users.noreply.github.com>
2014-09-23 02:50:34 +0200

    Add default value if MSVC_INSTALL_PATH is not defined

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,6 +1,14 @@
 mkdir build
 cd build
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+
+IF DEFINED MSVC_INSTALL_PATH (
+	ECHO MSVC_INSTALL_PATH is defined
+)  ELSE (
+	ECHO MSVC_INSTALL_PATH is NOT defined, using default
+	set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
+)
+
+call "%MSVC_INSTALL_PATH%\VC\vcvarsall.bat" x86
 
 cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
@@ -8,4 +16,3 @@ nmake
 nmake install
 cd install
 7z a BlobbyThing.zip bin
-pause
