5881b91af1febb4858ae03a61d1ad75785d5ae29
Torsten Stremlau <Torsten Stremlau@localhost>
2014-09-23 02:35:11 +0200

    Add (and use) some 3rd_party libs

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,15 +1,11 @@
 mkdir build
 cd build
-call "D:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
-
-set THIRD_PARTY_LIBDIR=D:\DevLibraries
-
-set SDL2DIR=%THIRD_PARTY_LIBDIR%\SDL2-2.0.3
-set BOOST_ROOT=%THIRD_PARTY_LIBDIR%\boost_1_56_0
-set ICU_ROOT=%THIRD_PARTY_LIBDIR%\icu-54.1-win32-release
-set GLEWDIR=%THIRD_PARTY_LIBDIR%\glew-1.11.0
-set SDL2IMAGEDIR=%THIRD_PARTY_LIBDIR%\SDL2_image-2.0.0
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
-rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_BUILD_TYPE=Release
+rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 nmake
+nmake install
+cd install
+7z a BlobbyThing.zip bin
+pause
