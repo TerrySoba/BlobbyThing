269a48e2a2d393f410868ed04d716197afa5fdd0
Torsten Stremlau <Torsten Stremlau@localhost>
2014-10-01 01:00:57 +0200

    use make instead of ninja

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,8 +1,8 @@
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/install
-ninja
-ninja install
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/install
+make -j 4
+make install
 cd install
 7z a ${JOB_NAME}_${BUILD_ID}.zip bin
 
