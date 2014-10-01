b479da2dd27fcaa5baff82ffcf851f872c1cb9b1
Torsten Stremlau <TerrySoba@users.noreply.github.com>
2014-10-01 05:38:17 +0200

    try something else...

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -8,7 +8,7 @@ IF DEFINED MSVC_INSTALL_PATH (
 	set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
 )
 
-call %MSVC_INSTALL_PATH%\VC\vcvarsall.bat x86
+call "%MSVC_INSTALL_PATH%\VC\vcvarsall.bat" x86
 
 cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
