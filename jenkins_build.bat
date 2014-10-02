3c2b4aacf1c90d1ba45f7a34fdc5cc1fee528abe
Torsten Stremlau <torsten@stremlau.eu>
2014-10-03 00:23:05 +0200

    Here we go again :)

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,14 +1,14 @@
 mkdir build
 cd build
 
-rem IF DEFINED MSVC_INSTALL_PATH (
-rem ECHO MSVC_INSTALL_PATH is defined
-rem )  ELSE (
-rem ECHO MSVC_INSTALL_PATH is NOT defined, using default
-rem set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
-rem )
+IF DEFINED MSVC_INSTALL_PATH (
+ECHO MSVC_INSTALL_PATH is defined
+)  ELSE (
+ECHO MSVC_INSTALL_PATH is NOT defined, using default
+set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
+)
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call "%MSVC_INSTALL_PATH%\VC\vcvarsall.bat" x86
 
 cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
