5d6335cb1725c213317eee55c866539cbdcfc7b7
Torsten Stremlau <Torsten Stremlau@localhost>
2014-10-03 05:16:58 +0200

    Escape batch correctly... (windows is weird...)

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -6,10 +6,10 @@ IF DEFINED MSVC_INSTALL_PATH (
 ECHO MSVC_INSTALL_PATH is defined
 )  ELSE (
 ECHO MSVC_INSTALL_PATH is NOT defined, using default
-set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
+set MSVC_INSTALL_PATH=C:\Program Files ^(x86^)\Microsoft Visual Studio 12.0
 )
 
-call %MSVC_INSTALL_PATH%\VC\vcvarsall.bat x86
+call "%MSVC_INSTALL_PATH%\VC\vcvarsall.bat" x86
 
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
