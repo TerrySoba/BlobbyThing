6eb16644b60d4cb8d813b6291f353ba82263e3b5
Torsten Stremlau <Torsten Stremlau@localhost>
2014-10-03 05:04:55 +0200

    Use ninja instead of nmake

--- jenkins_build.bat
+++ jenkins_build.bat
@@ -1,4 +1,5 @@
 mkdir build
+copy 3rd_party\windows\ninja\ninja.exe build
 cd build
 
 IF DEFINED MSVC_INSTALL_PATH (
@@ -8,11 +9,11 @@ ECHO MSVC_INSTALL_PATH is NOT defined, using default
 set MSVC_INSTALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
 )
 
-call "%MSVC_INSTALL_PATH%\VC\vcvarsall.bat" x86
+call %MSVC_INSTALL_PATH%\VC\vcvarsall.bat x86
 
-cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
 rem cmake .. -G "Visual Studio 12 2013" -DCMAKE_INSTALL_PREFIX:PATH=%CD%/install
-nmake
-nmake install
+ninja
+ninja install
 cd install
 7z a %JOB_NAME%_%BUILD_ID%.zip bin
