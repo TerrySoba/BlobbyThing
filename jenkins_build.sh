4d00a0bd8c149269901ddbc00ba40c8b2de3bec9
Torsten Stremlau <torsten@stremlau.eu>
2014-10-07 22:07:31 +0200

    Add version number

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/install
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/install -DBLOBBY_VERSION="Build ${MERCURIAL_REVISION_SHORT}:${BUILD_NUMBER}"
 make -j 4
 make install
 cd install
